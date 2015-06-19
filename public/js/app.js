e8210e02c5cd749fcddfdf601cc7e4eeb8dd818c
Alex Gitonga <sachgits@yahoo.com>
2015-06-19 13:40:58 -0700

    second branch app could not work because watchify does not recognise render() as shortcut to making function inside of object rectified this to render: function(){}

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,11 @@
  * Created by accutech on 6/17/15.
  */
 
+    var React = require('react');
 var Router = require('react-router');
 var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
+var RouteHandler = Router.RouteHandler;
 
 var Main = require('./components/App');
 var Popular = require('./components/Popular');
@@ -20,6 +22,25 @@ var routes = (
             <Route path="login" handler={Login} />
             <Route path="Register" handler={Register} />
         </Route>
+        <Route path="Collection" handler={Collection} />
+        <DefaultRoute handler={Main} />
     </Route>
 );
+
+var Application = React.createClass({
+    render: function(){
+        return (
+            <div>
+            <h2>Jambotunes</h2>
+                <ul>
+                    <li><Link to="Popular" >Most Popular</Link></li>
+                    <li><Link to="Collection" >My Collections</Link></li>
+                    <li><Link to="Membership" >Membership</Link></li>
+                    <li><Link to="Membership/Login" >Login</Link></li>
+                </ul>
+                <RouteHandler></RouteHandler>
+            </div>
+        )
+    }
+});
 
